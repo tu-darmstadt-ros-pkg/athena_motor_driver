a9ae66d6375515f4a4101d0eec3a6f553ae87c0e
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-03-07 17:01:11 +0100

    Updated status led to use time.

--- athena_motor_firmware/src/status_led.h
+++ athena_motor_firmware/src/status_led.h
@@ -4,13 +4,13 @@
 
 // LED
 struct StatusLED {
-  enum Speed { SLOW = 2000 * 1000 / MAIN_LOOP_DELAY_IN_US, FAST = 200 * 1000 / MAIN_LOOP_DELAY_IN_US };
+  enum Speed { SLOW = 1000, FAST = 200 };
   
   StatusLED( int pin ) : led_pin_( pin ) { pinMode( pin, OUTPUT ); }
 
   void update() {
-    if ( ++led_counter >= speed ) {
-      led_counter = 0;
+    if ( last_toggle >= speed ) {
+      last_toggle = 0;
       last_led_state = !last_led_state;
       digitalWrite( led_pin_, last_led_state ? LOW : HIGH );
     }
@@ -18,6 +18,7 @@ struct StatusLED {
 
   int led_pin_;
   int led_counter = 0;
+  elapsedMillis last_toggle;
   bool last_led_state = false;
   Speed speed = SLOW;
 } status_led( LED_BUILTIN );
